339505c171d0e74e1dfa9640e6eaed74045cf2b1
curtesmith <curt.e.smith@gmail.com>
2015-11-28 12:03:42 -0500

    Added call to discoverPeers from the action bar

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/MasterActivity.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/MasterActivity.java
@@ -1,16 +1,26 @@
 package ca.brocku.cosc3p97.bigbuzzerquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MasterActivity extends AppCompatActivity {
+    private WifiP2pManager manager;
+    private Channel channel;
+    private static final String TAG = "MasterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
     }
 
     @Override
@@ -27,9 +37,22 @@ public class MasterActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_discover:
+                manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.i(TAG, "call to discoverPeers was successful");
+                    }
+
+                    @Override
+                    public void onFailure(int i) {
+                        Log.e(TAG, "call to discoverPeers failed, code = " + i);
+                    }
+                });
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
