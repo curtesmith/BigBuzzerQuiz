7c2a3570887018832bb44dbc7ee9f51463c3f535
curtesmith <curt.e.smith@gmail.com>
2015-11-29 12:01:34 -0500

    add dataset observer

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/ListDataSetObserver.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/ListDataSetObserver.java
@@ -1,5 +1,25 @@
 package ca.brocku.cosc3p97.bigbuzzerquiz;
 
 
-public class ListDataSetObserver {
+import android.database.DataSetObserver;
+import android.util.Log;
+
+public class ListDataSetObserver extends DataSetObserver{
+    private static final String TAG = "...DataSetObserver";
+    private MasterActivity activity;
+
+
+    public ListDataSetObserver(MasterActivity activity) {
+        this.activity = activity;
+    }
+
+
+    public void onChanged() {
+        activity.updateSelectedPlayers();
+    }
+
+
+    public void onInvalidated() {
+        Log.i(TAG, "onInvalidated called");
+    }
 }
