78751dc0b16aadc1641a3564929b5eef4f844a75
Maike Rees <maikefer_@gmx.de>
2015-12-01 15:14:06 -0500

    add startPlayerActivity

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/StartPlayerActivity.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/StartPlayerActivity.java
@@ -1,24 +1,22 @@
 package ca.brocku.cosc3p97.bigbuzzerquiz;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class StartActivity extends AppCompatActivity {
+public class StartPlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_start_player);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start, menu);
+        getMenuInflater().inflate(R.menu.menu_start_player, menu);
         return true;
     }
 
@@ -36,15 +34,4 @@ public class StartActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void choseMasterMode(View view){
-        Intent intent = new Intent(this, MasterActivity.class);
-        startActivity(intent);
-    }
-
-    public void chosePlayerMode(View view) {
-        Intent intent = new Intent(this, StartPlayerActivity.class);
-        startActivity(intent);
-    }
-
 }
