cd9d059c078064a17633363a1e9cd9b6929811cc
curtesmith <curt.e.smith@gmail.com>
2015-12-13 15:58:15 -0500

    Added comments to the JsonMessage class

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/common/JsonMessage.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/common/JsonMessage.java
@@ -4,15 +4,30 @@ package ca.brocku.cosc3p97.bigbuzzerquiz.messages.common;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+/**
+ * Base class from which to build the request and response messages which extends the
+ * JSONObject class
+ */
 public class JsonMessage extends JSONObject {
     public static final String TYPE = "TYPE";
     public static final String IDENTIFIER = "IDENTIFIER";
 
+
+    /**
+     * A constructor which exposes the JSONObject super class constructor
+     * @param string
+     * @throws JSONException
+     */
     public JsonMessage(String string) throws JSONException {
         super(string);
     }
 
 
+    /**
+     * A separate constructor the will impose that any derived class will implement the
+     * setType and setIdentifier methods
+     */
     public JsonMessage() {
         super();
         setType();
@@ -20,6 +35,10 @@ public class JsonMessage extends JSONObject {
     }
 
 
+    /**
+     * Retrieves the TYPE attribute
+     * @return
+     */
     public String getType() {
         try {
             return getString(TYPE);
@@ -31,11 +50,19 @@ public class JsonMessage extends JSONObject {
     }
 
 
+    /**
+     * Intended to set the TYPE attribute of the object which any derived class must override
+     * and implement
+     */
     protected void setType() {
         throw new RuntimeException("Stub");
     }
 
 
+    /**
+     * Retrieves the IDENTIFIER attribute
+     * @return
+     */
     public String getIdentifier() {
         try {
             return getString(IDENTIFIER);
@@ -47,6 +74,10 @@ public class JsonMessage extends JSONObject {
     }
 
 
+    /**
+     * Intended to set the IDENTIFIER attribute of the object which any derived class must
+     * override
+     */
     protected void setIdentifier() {
         throw new RuntimeException("Stub");
     }
