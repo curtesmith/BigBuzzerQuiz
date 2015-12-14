c3114d6e9cdf44642c8950f55834f8674efab888
curtesmith <curt.e.smith@gmail.com>
2015-12-13 21:17:25 -0500

    Added GameContract class

---
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/common/GameContract.java
@@ -0,0 +1,9 @@
+package ca.brocku.cosc3p97.bigbuzzerquiz.messages.common;
+
+/**
+ * Created by Curtis on 2015-12-13.
+ */
+public class GameContract {
+    public int numberOfQuestions;
+    public int[] categoryKeys;
+}
