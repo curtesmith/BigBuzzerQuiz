738f3793e3f1b739cd0edab1735457cc9bd19793
curtesmith <curt.e.smith@gmail.com>
2015-12-13 21:23:02 -0500

    Added comments to the ReadyRequest and ReadyRequestHandler classes

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/ReadyRequest.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/ReadyRequest.java
@@ -6,16 +6,35 @@ import org.json.JSONException;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.JsonMessage;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Request;
 
+
+/**
+ * A request object responsible for sending a request to the host from the player to inform
+ * the host that the player is ready for the next question of the game
+ */
 public class ReadyRequest extends Request {
+
+    /**
+     * Constructor which uses the constructor of the super class that allows the internal
+     * JSONObject of this class to be populated from the JSON string passed as an argument
+     * @param string JSON string used to load this instance
+     * @throws JSONException
+     */
     public ReadyRequest(String string) throws JSONException {
         super(string);
     }
 
+
+    /**
+     * Constructor
+     */
     public ReadyRequest() {
         super();
     }
 
 
+    /**
+     * Override the setIdentifier method with the value READY
+     */
     public void setIdentifier() {
         try {
             put(JsonMessage.IDENTIFIER, HostRequestContract.READY);
--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/ReadyRequestHandler.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/ReadyRequestHandler.java
@@ -7,20 +7,39 @@ import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Request;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Sender;
 import ca.brocku.cosc3p97.bigbuzzerquiz.models.Host;
 
+/**
+ * Responsible for handling the incoming ready request from the player
+ */
 public class ReadyRequestHandler  extends HostRequestHandler {
     private static final String TAG = "PlayRequestHandler";
 
+
+    /**
+     * A constructor which takes a reference to the host that it is serving
+     * @param host the host which this handler is serving
+     */
     public ReadyRequestHandler(Host host) {
         super(host);
     }
 
 
+    /**
+     * Handle the request passed as an argument. If a response is necessary then it can be
+     * built and then passed back the calling player using the replyToSender reference passed
+     * as an argument.
+     * @param request the request details
+     * @param replyToSender a reference to the player that is making the request
+     */
     @Override
     public void handle(Request request, Sender replyToSender) {
         ready(replyToSender);
     }
 
 
+    /**
+     * Make a call to the host to let inform that the player is ready.
+     * @param sender
+     */
     public void ready(Sender sender) {
         Log.i(TAG, "ready: invoked");
         host.ready();
