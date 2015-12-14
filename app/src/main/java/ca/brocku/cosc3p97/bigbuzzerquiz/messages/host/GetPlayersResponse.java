5586becaedb356d7a18e985bab3e49b32431109a
curtesmith <curt.e.smith@gmail.com>
2015-12-13 20:27:49 -0500

    Added comments to the GetPlayersResponse and GetPlayersResponseHandler classes

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/GetPlayersResponse.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/GetPlayersResponse.java
@@ -10,20 +10,37 @@ import java.util.List;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.JsonMessage;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Response;
 
+/**
+ * A response object responsible for sending a response to the player from the host responding
+ * with a the list of names of the players that are connected to the host.
+ */
 public class GetPlayersResponse extends Response {
     public static final String NAMES = "NAMES";
 
 
+    /**
+     * Constructor which exposes the constructor of the super class and builds the internal
+     * JSONObject frpm the string passed in as an argument
+     * @param string JSON string of values
+     * @throws JSONException
+     */
     public GetPlayersResponse(String string) throws JSONException {
         super(string);
     }
 
+
+    /**
+     * Constructor that calls an override for the setIdentifier method
+     */
     public GetPlayersResponse() {
         super();
         setIdentifier();
     }
 
 
+    /**
+     * Override the setIdentifier method with the value GET_PLAYERS
+     */
     @Override
     public void setIdentifier() {
         try {
@@ -34,11 +51,36 @@ public class GetPlayersResponse extends Response {
     }
 
 
+    /**
+     * Create the NAMES attribute and assign to it the value passed as an argument
+     * @param toBeSerialized the boolean value to assign to the attribute
+     */
     @Override
-    public Object getResult() {
+    public void serialize(Object toBeSerialized) {
+        List<String> names = (List<String>) toBeSerialized;
+
+        try {
+            JSONArray jsonNames = new JSONArray();
+            for (String name : names) {
+                jsonNames.put(name);
+            }
+
+            put(GetPlayersResponse.NAMES, jsonNames);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Convert the internal value of the NAMES attribute into a List<String> value and return
+     * it to the calling method
+     */
+    @Override
+    public Object deserialize() {
         List<String> names = new ArrayList<>();
         try {
-            JSONArray jsonNames = getJSONArray("NAMES");
+            JSONArray jsonNames = getJSONArray(GetPlayersResponse.NAMES);
 
             for (int i = 0; i < jsonNames.length(); i++) {
                 names.add((String) jsonNames.get(i));
@@ -51,19 +93,4 @@ public class GetPlayersResponse extends Response {
     }
 
 
-    @Override
-    public void serialize(Object toBeSerialized) {
-        List<String> names = (List<String>) toBeSerialized;
-
-        try {
-            JSONArray jsonNames = new JSONArray();
-            for (String name : names) {
-                jsonNames.put(name);
-            }
-
-            put("NAMES", jsonNames);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
 }
--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/GetPlayersResponseHandler.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/host/GetPlayersResponseHandler.java
@@ -3,31 +3,54 @@ package ca.brocku.cosc3p97.bigbuzzerquiz.messages.host;
 
 import org.json.JSONException;
 
+import java.util.List;
+
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Response;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Sender;
 import ca.brocku.cosc3p97.bigbuzzerquiz.models.Player;
 
+/**
+ * Responsible for handling the incoming answer response from the host
+ */
 public class GetPlayersResponseHandler extends HostResponseHandler {
 
+
+    /**
+     * A constructor which takes a reference to the player that it is serving
+     * @param player the player which this handler is serving
+     */
     public GetPlayersResponseHandler(Player player) {
         super(player);
     }
 
 
+    /**
+     * Handle the request passed as an argument. If a response is necessary then it can be
+     * built and then passed back the calling player using the replyToSender reference passed
+     * as an argument.
+     * Return the list of players from the host, place them in a response object and then
+     * deserialize them into a list<String> for the calling object to consume.
+     * @param response the response details
+     * @param replyToSender a reference to the host that is sending the response
+     */
     @Override
     public void handle(Response response, Sender replyToSender) {
-        getPlayers(response);
-    }
-
-
-    private void getPlayers(Response response) {
         try {
             GetPlayersResponse getPlayersResponse = new GetPlayersResponse(response.toString());
-            callback.reply(getPlayersResponse.getResult());
+            List<String> names = (List<String>) getPlayersResponse.deserialize();
+            respond(names);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
 
+
+    /**
+     * Invoke the reply method of the callback object that is expecting the response from the host.
+     * @param names the list of names that were requested
+     */
+    private void respond(List<String> names) {
+        callback.reply(names);
     }
 
 }
