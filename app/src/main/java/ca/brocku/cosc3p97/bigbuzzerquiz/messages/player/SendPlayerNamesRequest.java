2669a023496fef59da105d4208da4b3b17cdc806
curtesmith <curt.e.smith@gmail.com>
2015-12-14 21:37:09 -0500

    Added comments to SendPlayerNamesRequest and SendPlayerNamesRequestHandler classes

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/SendPlayerNamesRequest.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/SendPlayerNamesRequest.java
@@ -10,19 +10,35 @@ import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.JsonMessage;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Request;
 
 
+/**
+ * A request object responsible for sending a request to the player from the host to inform
+ * the player that the list of players has been updated
+ */
 public class SendPlayerNamesRequest extends Request {
     public static final String NAMES = "NAMES";
 
 
+    /**
+     * Constructor which uses the constructor of the super class that allows the internal
+     * JSONObject of this class to be populated from the JSON string passed as an argument
+     * @param string JSON string used to load this instance
+     * @throws JSONException
+     */
     public SendPlayerNamesRequest(String string) throws JSONException {
         super(string);
     }
 
+    /**
+     * Constructor
+     */
     public SendPlayerNamesRequest() {
         super();
     }
 
 
+    /**
+     * Override the setIdentifier method with the value READY
+     */
     public void setIdentifier() {
         try {
             put(JsonMessage.IDENTIFIER, PlayerMessageContract.SEND_PLAYER_NAMES);
@@ -32,7 +48,13 @@ public class SendPlayerNamesRequest extends Request {
     }
 
 
-    public void serialize(List<String> names) {
+    /**
+     * Create the NAMES attribute and assign to it the value passed as an argument
+     * @param toBeSerialized the boolean value to assign to the attribute
+     */
+    @Override
+    public void serialize(Object toBeSerialized) {
+        List<String> names = (List<String>) toBeSerialized;
         try {
             JSONArray jsonNames = new JSONArray();
             for(String name : names) {
@@ -45,7 +67,12 @@ public class SendPlayerNamesRequest extends Request {
     }
 
 
-    public List<String> deserialize() {
+    /**
+     * Convert the internal value of the NAMES attribute into a List<String>
+     * value and return it to the calling method
+     */
+    @Override
+    public Object deserialize() {
         List<String> names = new ArrayList<>();
         try {
             JSONArray jsonNames = getJSONArray(SendPlayerNamesRequest.NAMES);
--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/SendPlayerNamesRequestHandler.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/SendPlayerNamesRequestHandler.java
@@ -3,21 +3,40 @@ package ca.brocku.cosc3p97.bigbuzzerquiz.messages.player;
 
 import org.json.JSONException;
 
+import java.util.List;
+
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Request;
 import ca.brocku.cosc3p97.bigbuzzerquiz.messages.common.Sender;
 import ca.brocku.cosc3p97.bigbuzzerquiz.models.Player;
 
+
+/**
+ * Responsible for handling the incoming send player names request from the host
+ */
 public class SendPlayerNamesRequestHandler extends PlayerRequestHandler {
+
+    /**
+     * A constructor which takes a reference to the player that it is serving
+     * @param player the player which this handler is serving
+     */
     public SendPlayerNamesRequestHandler(Player player) {
         super(player);
     }
 
 
+    /**
+     * Handle the request passed as an argument. If a response is necessary then it can be
+     * built and then passed back the calling host using the replyToSender reference passed
+     * as an argument. This method will call the updatePlayerNames method of the player passing the
+     * arguments passed in the request object.
+     * @param request the request details
+     * @param replyToSender a reference to the host that is making the request
+     */
     @Override
     public void handle(Request request, Sender replyToSender) {
         try {
             SendPlayerNamesRequest sendPlayerNamesRequest = new SendPlayerNamesRequest(request.toString());
-            player.updatePlayerNames(sendPlayerNamesRequest.deserialize());
+            player.updatePlayerNames((List<String>)sendPlayerNamesRequest.deserialize());
         } catch (JSONException e) {
             e.printStackTrace();
         }
