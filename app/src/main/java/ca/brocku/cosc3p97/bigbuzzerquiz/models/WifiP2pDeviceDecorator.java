4acb920ce16f8f2c99a5b6a64888d897d70f04e1
curtesmith <curt.e.smith@gmail.com>
2015-12-14 22:21:15 -0500

    Added comments to the WifiP2pDeviceDecorator class

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/models/WifiP2pDeviceDecorator.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/models/WifiP2pDeviceDecorator.java
@@ -7,13 +7,22 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Responsible for extending the WifiP2pDevice class with additional information needed by
+ * the application
+ */
 public class WifiP2pDeviceDecorator extends WifiP2pDevice {
     private static final String TAG = "WifiP2pDeviceDecorator";
     public boolean isConnecting;
     public boolean isSelected;
     public boolean isEnabled;
 
+
+    /**
+     * Constructor
+     * @param source is a WifiP2pDevice instance that is to be used as a source
+     *               for this instance of the class
+     */
     public WifiP2pDeviceDecorator(WifiP2pDevice source) {
         super(source);
         Log.i(TAG, "ctor name={" + source.deviceName + "}, isGroupOwner={" + source.isGroupOwner() + "}");
@@ -23,11 +32,23 @@ public class WifiP2pDeviceDecorator extends WifiP2pDevice {
     }
 
 
+    /**
+     * Compare this instance to that of the instance passed as an argument and response with
+     * a boolean value of true if the two instances match otherwise return a value of false
+     * @param device an instance to compare to
+     * @return boolean true = there is a match, false = no match
+     */
     public boolean matches(WifiP2pDevice device) {
         return deviceName.equals(device.deviceName);
     }
 
 
+    /**
+     * Copy the values in the "from" argument to the destination of the "to" argument
+     * @param from source argument
+     * @param to destination argument
+     * @return an updated copy of the destination argument with the updates from the source
+     */
     public static List<WifiP2pDeviceDecorator> copy(WifiP2pDeviceList from, List<WifiP2pDeviceDecorator> to) {
         ArrayList<WifiP2pDeviceDecorator> list = new ArrayList<>();
 
@@ -44,6 +65,11 @@ public class WifiP2pDeviceDecorator extends WifiP2pDevice {
     }
 
 
+    /**
+     * Update the current instance from the information of a matching instance from the list
+     * provided as an argument
+     * @param source the list of devices from which to find a match and update
+     */
     private void update(List<WifiP2pDeviceDecorator> source) {
         for(WifiP2pDeviceDecorator device : source) {
             if(matches(device)) {
@@ -56,16 +82,5 @@ public class WifiP2pDeviceDecorator extends WifiP2pDevice {
                 isSelected = device.isSelected;
             }
         }
-    }
-
-
-    public static int countSelected(List<WifiP2pDeviceDecorator> list) {
-        int count = 0;
-        for(WifiP2pDeviceDecorator device : list) {
-            if (device.isSelected) {
-                count++;
-            }
-        }
-        return count;
     }
 }
