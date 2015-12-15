b718c2a233065f1b941247b2750b25601c935d67
curtesmith <curt.e.smith@gmail.com>
2015-12-14 22:04:42 -0500

    Added comments to PlayerProxy and PlayerRequestHandler classes

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerProxy.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerProxy.java
@@ -20,12 +20,22 @@ import ca.brocku.cosc3p97.bigbuzzerquiz.models.Host;
 import ca.brocku.cosc3p97.bigbuzzerquiz.models.Participant;
 
 
+/**
+ * Responsible for managing the requests from the host to the player and from the player back to the
+ * host
+ */
 public class PlayerProxy implements PlayerActions {
     private static final String TAG = "PlayerProxy";
     private PlayerConnection connection;
     private HashMap<String, RequestHandler> requestHandlers = new HashMap<>();
 
 
+    /**
+     * Constructor which takes a reference of the host itself as well as a listener which
+     * will be invoked when the onSetup event is fired from the player's connection handler
+     * @param host a reference to the host object
+     * @param listener a reference to the onSetup listener
+     */
     public PlayerProxy(Host host, PlayerConnection.SetupListener listener) throws Exception {
         Log.i(TAG, "ctor: invoked");
         connection = new PlayerConnection(host, listener);
@@ -33,33 +43,63 @@ public class PlayerProxy implements PlayerActions {
     }
 
 
+    /**
+     * Add a request handler for a specific request from the player
+     * @param key id of the request
+     * @param requestHandler a reference to the handler that will be be invoked when a matching
+     *                             request is made
+     */
     public void addRequestHandler(String key, RequestHandler requestHandler) {
         requestHandlers.put(key, requestHandler);
     }
 
 
+    /**
+     * Assign a listener that can be invoked when specific Tcp Connection events occur
+     * @param listener a reference to the Tcp connection listener
+     */
     public void setPlayerTcpListener(TcpConnection.Listener listener) {
         connection.setPlayerTcpListener(listener);
     }
 
 
+    /**
+     * Retrieve a reference to the thread handler associated with the underlying connection
+     * @return the connection thread handler
+     */
     public Handler getThreadHandler() {
         return connection.getThreadHandler();
     }
 
 
+    /**
+     * Get the player list index of the player in the list that is associated with the Sender
+     * reference that is passed in as an argument
+     * @param sender the Sender reference of this player proxy
+     * @return an integer that is the index of this player in the player list
+     */
     public int getPlayerIndex(Sender sender) {
         TcpConnection conn = (TcpConnection) sender;
         return connection.getConnectionIndex(conn);
     }
 
 
+    /**
+     * Search through and invoke the appropriate host request handler that is associated
+     * with the request identifier
+     * @param request request details
+     * @param replyToSender a reference to the host connection where the request was read
+     */
     public void handleHostRequest(Request request, Sender replyToSender) {
         requestHandlers.get(request.getIdentifier())
                 .handle(request, replyToSender);
     }
 
 
+    /**
+     * Makes a call to the player to present the question details
+     * @param question contains the question details
+     */
     @Override
     public void showQuestion(QuestionContract question) {
         Log.i(TAG, "showQuestion: invoked");
@@ -72,6 +112,10 @@ public class PlayerProxy implements PlayerActions {
     }
 
 
+    /**
+     * Makes a call to the player to update the list of players
+     * @param players this list of players
+     */
     public void sendPlayerNames(List<Participant> players) {
         List<String> names = new ArrayList<>();
         for (Participant participant : players) {
@@ -84,6 +128,9 @@ public class PlayerProxy implements PlayerActions {
     }
 
 
+    /**
+     * Makes a call to the player to present the a timeout alert
+     */
     @Override
     public void timeout() {
         Log.i(TAG, "timeout: invoked");
@@ -98,6 +145,11 @@ public class PlayerProxy implements PlayerActions {
     }
 
 
+    /**
+     * Makes a call to the player to present that someone has already answered the question
+     * successfully
+     * @param playerName the name of the player that answered the question correctly
+     */
     public void success(String playerName) {
         Log.i(TAG, "success: invoked");
         InterruptRequest request = new InterruptRequest();
@@ -112,6 +164,10 @@ public class PlayerProxy implements PlayerActions {
     }
 
 
+    /**
+     * Makes a call to the player to present the game over information
+     * @param players the results of each player in the game
+     */
     @Override
     public void gameOver(List<Participant> players) {
         Log.i(TAG, String.format("gameOver: invoked with %s players", players.size()));
@@ -122,6 +178,10 @@ public class PlayerProxy implements PlayerActions {
     }
 
 
+    /**
+     * Makes a call to the player to inform them that all participants failed to
+     * answer the question successfully
+     */
     @Override
     public void everyoneFailed() {
         Log.i(TAG, "everyone: invoked");
--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerRequestHandler.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerRequestHandler.java
@@ -12,11 +12,23 @@ import ca.brocku.cosc3p97.bigbuzzerquiz.models.Player;
 public class PlayerRequestHandler extends RequestHandler {
     Player player;
 
+
+    /**
+     * A constructor which takes a reference to the player that it is serving
+     * @param player the player which this handler is serving
+     */
     public PlayerRequestHandler(Player player) {
         this.player = player;
     }
 
 
+    /**
+     * Handle the request passed as an argument. If a response is necessary then it can be
+     * built and then passed back the calling host using the replyToSender reference passed
+     * as an argument.
+     * @param request the request details
+     * @param replyToSender a reference to the host that is making the request
+     */
     @Override
     public void handle(Request request, Sender replyToSender) {
         throw new RuntimeException("Stub");
