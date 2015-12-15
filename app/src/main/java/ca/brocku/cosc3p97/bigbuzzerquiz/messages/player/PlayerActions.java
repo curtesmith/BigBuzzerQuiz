65ed4b771dfe4c78e32e7fdeb7f1490031e75962
curtesmith <curt.e.smith@gmail.com>
2015-12-14 21:28:16 -0500

    Added comments to PlayerActions and PlayerMessageContract files

--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerActions.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerActions.java
@@ -6,6 +6,9 @@ import java.util.List;
 import ca.brocku.cosc3p97.bigbuzzerquiz.database.QuestionContract;
 import ca.brocku.cosc3p97.bigbuzzerquiz.models.Participant;
 
+/**
+ * An interface that defines the actions that the host will request to a player
+ */
 public interface PlayerActions {
     void showQuestion(QuestionContract question);
     void timeout();
--- app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerMessageContract.java
+++ app/src/main/java/ca/brocku/cosc3p97/bigbuzzerquiz/messages/player/PlayerMessageContract.java
@@ -1,6 +1,10 @@
 package ca.brocku.cosc3p97.bigbuzzerquiz.messages.player;
 
 
+/**
+ * An interface the defines constants for the messages that can be sent from the host
+ * to a player
+ */
 public interface PlayerMessageContract {
     String SHOW_QUESTION = "SHOW_QUESTION";
     String INTERRUPT = "INTERRUPT";
